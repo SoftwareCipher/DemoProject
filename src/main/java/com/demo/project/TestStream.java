031f0f42ebc023b3956826700dd40c2ac5b2ef2b
observerUA <kirillkuznetsov181@gmail.com>
2024-12-04 02:19:03 +0200

    stream tasks

--- src/main/java/com/demo/project/TestStream.java
+++ src/main/java/com/demo/project/TestStream.java
@@ -9,13 +9,20 @@ import java.util.stream.Collectors;
 public class TestStream {
     public static List<Account> accounts = Arrays.asList(
             new Account(1L, "Justin", "Butler", "justin.butler@gmail.com",
-                    LocalDate.parse("2003-04-17"), Account.Sex.MALE, LocalDate.parse("2016-06-13"), BigDecimal.valueOf(172966)),
+                    LocalDate.parse("2003-04-17"), Account.Sex.MALE, LocalDate.parse("2016-06-13"),
+                    BigDecimal.valueOf(3_000_000_000L)),
+
             new Account(2L, "Olivia", "Cardenas", "cardenas@mail.com",
-                    LocalDate.parse("1930-01-19"), Account.Sex.FEMALE, LocalDate.parse("2014-06-21"), BigDecimal.valueOf(38029)),
+                    LocalDate.parse("1930-01-19"), Account.Sex.FEMALE, LocalDate.parse("2014-06-21"),
+                    BigDecimal.valueOf(38029)),
+
             new Account(3L, "Nolan", "Donovan", "nolandonovan@gmail.com",
-                    LocalDate.parse("1925-04-19"), Account.Sex.MALE, LocalDate.parse("2011-03-10"), BigDecimal.valueOf(13889)),
+                    LocalDate.parse("1925-04-19"), Account.Sex.MALE, LocalDate.parse("2011-03-10"),
+                    BigDecimal.valueOf(13889)),
+
             new Account(4L, "Lucas", "Lynn", "lucas.lynn@yahoo.com",
-                    LocalDate.parse("1987-05-25"), Account.Sex.MALE, LocalDate.parse("2009-03-05"), BigDecimal.valueOf(16980))
+                    LocalDate.parse("1987-05-25"), Account.Sex.MALE, LocalDate.parse("2009-03-05"),
+                    BigDecimal.valueOf(16980))
     );
 
     public Optional<Account> findRichestPerson() {
@@ -41,13 +48,86 @@ public class TestStream {
                         .substring(account.getEmail().indexOf("@") + 1)));
     }
 
-    public int getNumOfLettersInFirstAndLastNames(){
+    public int getNumOfLettersInFirstAndLastNames() {
         return accounts.stream()
                 .mapToInt(value -> value.getFirstName().length() + value.getLastName().length())
                 .sum();
     }
 
-    public BigDecimal calculateTotalBalance(){
-        return BigDecimal.valueOf(accounts.stream().mapToInt(value -> value.getBalance().intValue()).sum());
+    //TODO
+    public BigDecimal calculateTotalBalance() {
+//        return BigDecimal.valueOf(accounts.stream().mapToInt(value -> value.getBalance().toBigInteger()).sum());
+
+        return accounts.stream()
+                .map(Account::getBalance)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public List<Account> sortByFirstAndLastNames() {
+        return accounts.stream()
+                .sorted(Comparator.comparing(Account::getFirstName)
+                        .thenComparing(Account::getLastName))
+                .toList();
+    }
+
+    public boolean containsAccountWithEmailDomain(String emailDomain) {
+        return accounts.stream()
+                .anyMatch(account -> account.getEmail()
+                .endsWith(emailDomain));
+    }
+
+    public BigDecimal getBalanceByEmail(String email) {
+        return accounts.stream()
+                .filter(account -> account.getEmail()
+                        .endsWith(email))
+                .map(Account::getBalance)
+                .findFirst()
+                .orElse(BigDecimal.ZERO);
+    }
+
+    public List<Account> getEmailsOfAccountsCreatedAfter(LocalDate localDate){
+        return accounts.stream()
+                .filter(account -> account.getCreationDate().isAfter(localDate))
+                .toList();
+    }
+
+    public Optional<Account> getOldestAccountHolder(){
+        return accounts.stream()
+                .min(Comparator.comparing(Account::getCreationDate));
+    }
+
+    public Optional<Account> getYoungestAccountHolder(){
+        return accounts.stream()
+                .max(Comparator.comparing(Account::getCreationDate));
+    }
+
+    public List<Account> getAccountsWithBalanceGreaterThan(BigDecimal bigDecimal){
+        return accounts.stream()
+                .filter(account -> account.getBalance().compareTo(bigDecimal) > 0)
+                .toList();
+    }
+
+    public Map<String, BigDecimal> getTotalBalanceByDomain(){
+//        return accounts.stream()
+//                .collect(Collectors.groupingBy(Account::getEmail,
+//                        accounts.stream().map(Account::getBalance)
+//                                .reduce(BigDecimal.ZERO, BigDecimal::add)));
+
+
+
+        return accounts.stream()
+                .collect(Collectors.groupingBy(
+                        account -> account.getEmail().substring(account.getEmail().indexOf("@") + 1),
+                        Collectors.mapping(Account::getBalance,
+                                Collectors.reducing(BigDecimal.ZERO, BigDecimal::add))));
+    }
+
+    public Map<Account.Sex, List<String>> getEmailsByGenderWithBalanceGreaterThan(BigDecimal minBalance){
+        return accounts.stream()
+                .filter(account -> account.getBalance().compareTo(minBalance) > 0)
+                .collect(Collectors.groupingBy(
+                        Account::getSex,
+                        Collectors.mapping(Account::getEmail, Collectors.toList())
+                ));
     }
 }
--- src/test/java/com/demo/project/TestStreamTest.java
+++ src/test/java/com/demo/project/TestStreamTest.java
@@ -3,6 +3,7 @@ package com.demo.project;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.Month;
 import java.util.*;
 
@@ -54,6 +55,85 @@ class TestStreamTest {
 
     @Test
     void testCalculateTotalBalance(){
-        assertEquals(BigDecimal.valueOf(241864), testStream.calculateTotalBalance());
+        assertEquals(BigDecimal.valueOf(3000068898L), testStream.calculateTotalBalance());
+    }
+
+    @Test
+    void testSortByFirstAndLastNames(){
+        List<Account> list = new ArrayList<>(){};
+        list.add(TestStream.accounts.get(0));
+        list.add(TestStream.accounts.get(3));
+        list.add(TestStream.accounts.get(2));
+        list.add(TestStream.accounts.get(1));
+
+        assertEquals(list, testStream.sortByFirstAndLastNames());
+    }
+
+    @Test
+    void testContainsAccountWithEmailDomain(){
+
+        assertTrue(testStream.containsAccountWithEmailDomain("gmail.com"));
+        assertTrue(testStream.containsAccountWithEmailDomain("mail.com"));
+    }
+
+    @Test
+    void testGetBalanceByEmail(){
+        List<Account> list = new ArrayList<>();
+        list.add(TestStream.accounts.get(0));
+
+        assertEquals(list.get(0).getBalance(), testStream.getBalanceByEmail("gmail.com"));
+    }
+
+    @Test
+    void testGetEmailsOfAccountsCreatedAfter(){
+        List<Account> list = new ArrayList<>();
+        list.add(TestStream.accounts.get(0));
+        list.add(TestStream.accounts.get(1));
+
+        assertEquals(list, testStream.getEmailsOfAccountsCreatedAfter(
+                LocalDate.of(2014, 1, 1)));
+    }
+
+    @Test
+    void testGetOldestAccountHolder(){
+        Optional<Account> account = Optional.of(TestStream.accounts.get(3));
+
+        assertEquals(account, testStream.getOldestAccountHolder());
+    }
+
+    @Test
+    void testGetYoungestAccountHolder(){
+        Optional<Account> account = Optional.of(TestStream.accounts.getFirst());
+
+        assertEquals(account, testStream.getYoungestAccountHolder());
+    }
+
+    @Test
+    void testGetAccountsWithBalanceGreaterThan(){
+        List<Account> list = new ArrayList<>();
+        list.add(TestStream.accounts.get(0));
+        list.add(TestStream.accounts.get(1));
+
+        assertEquals(list, testStream.getAccountsWithBalanceGreaterThan(BigDecimal.valueOf(30000L)));
+    }
+
+    @Test
+    void testGetTotalBalanceByDomain(){
+        Map<String, BigDecimal> expected = new HashMap<>(3);
+        expected.put("gmail.com", BigDecimal.valueOf(3_000_013_889L));
+        expected.put("mail.com", BigDecimal.valueOf(38029L));
+        expected.put("yahoo.com", BigDecimal.valueOf(16980L));
+
+        assertEquals(expected, testStream.getTotalBalanceByDomain());
+    }
+
+    @Test
+    void testGetEmailsByGenderWithBalanceGreaterThan(){
+        Map<Account.Sex, List<String>> expected = new HashMap<>(1);
+        expected.put(Account.Sex.MALE, List.of(TestStream.accounts.get(0).getEmail(),
+                TestStream.accounts.get(3).getEmail()));
+        expected.put(Account.Sex.FEMALE, List.of(TestStream.accounts.get(1).getEmail()));
+
+        assertEquals(expected, testStream.getEmailsByGenderWithBalanceGreaterThan(BigDecimal.valueOf(15_000L)));
     }
 }
