095e76d0526c9a9f04309cddf99112d51bdb8f70
observerUA <kirillkuznetsov181@gmail.com>
2024-12-16 22:21:23 +0200

    MyStack

--- src/main/java/com/demo/project/MyStack.java
+++ src/main/java/com/demo/project/MyStack.java
@@ -2,6 +2,8 @@ package com.demo.project;
 
 import lombok.Getter;
 
+import java.util.NoSuchElementException;
+
 public class MyStack<T> {
     private Node<T> top;
     @Getter
@@ -16,7 +18,7 @@ public class MyStack<T> {
         }
     }
 
-    public void add(T elemet){
+    public void push(T elemet){
         Node<T> newNode = new Node<>(elemet);
         newNode.next = top;
         top = newNode;
@@ -29,7 +31,6 @@ public class MyStack<T> {
         for(int i = 0; i < index; i++){
             current = current.next;
         }
-
         return current.data;
     }
 
@@ -39,15 +40,44 @@ public class MyStack<T> {
         for(int i = 0; i < index; i++){
             current = current.next;
         }
-
         current.data = data;
     }
 
+    public T peek(){
+        if (isEmpty()) {
+            throw new NoSuchElementException("Stack is empty");
+        }
+        return top.data;
+    }
 
-    private boolean checkIndex(int index){
+    public T pop(){
+        if (isEmpty()) {
+            throw new NoSuchElementException("Stack is empty");
+        }
+        Node<T> removeNode = top;
+        top = removeNode.next;
+        size--;
+        return removeNode.data;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int search(T o){
+        Node<T> current = top;
+        for(int i = 0; i < size - 1; i++){
+            if(current.data.equals(o)){
+                return i;
+            }
+            current = current.next;
+        }
+        return -1;
+    }
+
+    private void checkIndex(int index){
         if(index < 0 || index >= size){
             throw new IndexOutOfBoundsException("The index extends beyond the stack");
         }
-        return true;
     }
 }
--- src/test/java/com/demo/project/MyStackTest.java
+++ src/test/java/com/demo/project/MyStackTest.java
@@ -1,8 +1,7 @@
 package com.demo.project;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import java.util.NoSuchElementException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyStackTest {
@@ -10,9 +9,9 @@ class MyStackTest {
     @Test
     void testAdd() {
         MyStack myStack = new MyStack();
-        myStack.add("Element_1");
-        myStack.add("Element_2");
-        myStack.add("Element_3");
+        myStack.push("Element_1");
+        myStack.push("Element_2");
+        myStack.push("Element_3");
 
         assertEquals(3, myStack.getSize());
     }
@@ -20,24 +19,95 @@ class MyStackTest {
     @Test
     void testGet() {
         MyStack myStack = new MyStack();
-        myStack.add("Element_1");
-        myStack.add("Element_2");
-        myStack.add("Element_3");
+        myStack.push("Element_1");
+        myStack.push("Element_2");
+        myStack.push("Element_3");
 
         assertEquals("Element_1", myStack.get(2));
     }
 
     @Test
+    void testGetInvalidIndex(){
+        MyStack myStack = new MyStack();
+        myStack.push("Element_1");
+
+        assertThrows(IndexOutOfBoundsException.class, () -> myStack.get(1));
+    }
+
+    @Test
     void testSet() {
         MyStack myStack = new MyStack();
-        myStack.add("Element_1");
-        myStack.add("Element_2");
-        myStack.add("Element_3");
+        myStack.push("Element_1");
+        myStack.push("Element_2");
+        myStack.push("Element_3");
 
         myStack.set(0, "Element_3_3");
 
         assertEquals("Element_3_3", myStack.get(0));
     }
 
+    @Test
+    void testPeek(){
+        MyStack myStack = new MyStack();
+        myStack.push("Element_1");
+        myStack.push("Element_2");
+        myStack.push("Element_3");
 
+        assertEquals(myStack.get(0), myStack.peek());
+        assertEquals(3, myStack.getSize());
+    }
+
+    @Test
+    void testPeekEmptyStack(){
+        MyStack<String> myStack = new MyStack<>();
+        assertThrows(NoSuchElementException.class, myStack::peek);
+    }
+
+    @Test
+    void testPopEmptyStack(){
+        MyStack<String> myStack = new MyStack<>();
+        assertThrows(NoSuchElementException.class, myStack::pop);
+    }
+
+    @Test
+    void testPop(){
+        MyStack myStack = new MyStack();
+        myStack.push("Element_1");
+        myStack.push("Element_2");
+        myStack.push("Element_3");
+
+        assertEquals("Element_3", myStack.pop());
+        assertEquals(2, myStack.getSize());
+        assertEquals("Element_2", myStack.get(0));
+    }
+
+    @Test
+    void testIsEmptyTrue(){
+        MyStack myStack = new MyStack();
+        assertTrue(myStack.isEmpty());
+    }
+
+    @Test
+    void testIsEmptyFalse(){
+        MyStack myStack = new MyStack();
+        myStack.push("Element_1");
+        assertFalse(myStack.isEmpty());
+    }
+
+    @Test
+    void testSearch(){
+        MyStack myStack = new MyStack();
+        myStack.push("Element_1");
+        myStack.push("Element_2");
+        myStack.push("Element_3");
+        myStack.push("Element_4");
+        myStack.push("Element_5");
+        myStack.push("Element_6");
+        myStack.push("Element_7");
+        myStack.push("Element_8");
+
+        assertEquals(3, myStack.search("Element_5"));
+        assertEquals(-1, myStack.search("Element_10"));
+        assertEquals(0, myStack.search("Element_8"));
+    }
 }
