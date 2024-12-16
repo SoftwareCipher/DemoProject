f4a4eea5db22bda787389a30c81efb9ef4859d27
observerUA <kirillkuznetsov181@gmail.com>
2024-12-17 00:47:44 +0200

    practice with stream API

--- src/main/java/com/demo/project/Stream/StreamOrder.java
+++ src/main/java/com/demo/project/Stream/StreamOrder.java
@@ -3,7 +3,9 @@ package com.demo.project.Stream;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class StreamOrder {
     public static List<Order> orders = Arrays.asList(
@@ -30,12 +32,31 @@ public class StreamOrder {
                             BigDecimal.valueOf(499.99), 4.6, LocalDate.parse("2020-11-12")),
                     new Product(3L, "Wireless Headphones", "Accessories",
                             BigDecimal.valueOf(199.99), 4.5, LocalDate.parse("2021-03-05"))),
-                    "Daisy Miller", "Completed", LocalDate.parse("2021-07-05"))
+                    "Daisy Miller", "Completed", LocalDate.parse("2021-07-05")),
+
+            new Order(6L, Arrays.asList(
+                    new Product(4L, "Electric Kettle", "Home Appliances",
+                            BigDecimal.valueOf(49.99), 4.2, LocalDate.parse("2019-11-20"))
+            ), "Frank Wright", "Pending", LocalDate.parse("2023-08-15"))
     );
 
     public List<Order> getCompletedOrders(){
         return orders.stream()
                 .filter(order -> order.getStatus().equals("Completed"))
                 .toList();
+    }
+
+    public long countProductsInPendingOrders(){
+        return orders.stream()
+                .filter(order -> order.getStatus().equals("Pending")).count();
+    }
+
+    public Optional<Order> getOrderWithHighestTotalValue(){
+        return orders.stream()
+                .max(Comparator.comparing(order ->
+                        order.getProducts().stream()
+                                .map(Product::getPrice)
+                                .reduce(BigDecimal.ZERO, BigDecimal::add)
+                ));
     }
 }
--- src/test/java/com/demo/project/Stream/StreamOrderTest.java
+++ src/test/java/com/demo/project/Stream/StreamOrderTest.java
@@ -3,14 +3,16 @@ package com.demo.project.Stream;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static com.demo.project.Stream.StreamOrder.orders;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StreamOrderTest {
 
-    private StreamOrder streamOrder = new StreamOrder();
+    private final StreamOrder streamOrder = new StreamOrder();
 
     @Test
     void testGetCompletedOrders() {
@@ -19,5 +21,20 @@ class StreamOrderTest {
         list.add(orders.get(3));
 
         assertEquals(list, streamOrder.getCompletedOrders());
+    }
+
+    @Test
+    void testCountProductsInPendingOrders(){
+        List<Order> orders =List.of(StreamOrder.orders.get(1), StreamOrder.orders.get(4));
+
+        assertEquals(orders.size(), streamOrder.countProductsInPendingOrders());
+        assertEquals("Pending", orders.getFirst().getStatus());
+    }
+
+    @Test
+    void testGetOrderWithHighestTotalValue(){
+        Optional<Order> order = Optional.of(orders.get(2));
+
+        assertEquals(order, streamOrder.getOrderWithHighestTotalValue());
     }
 }
