05f6e3d3097eb28f5a3ea64c6dcacd17969b2b80
observerUA <kirillkuznetsov181@gmail.com>
2025-01-03 23:12:51 +0200

    clear main

--- src/main/java/com/demo/project/Main.java
+++ src/main/java/com/demo/project/Main.java
@@ -10,31 +10,31 @@ public class Main {
 
     @SneakyThrows
     public static void main(String[] args) {
-
-        CompletableFuture.supplyAsync(() -> {
-                    System.out.println("Task running in: " + Thread.currentThread().getName());
-                    return 42;
-                }).thenApply(result -> {
-                    System.out.println("Transforming result: " + result);
-                    return result * 2;
-                }).thenApply(result -> {
-                    System.out.println("Transforming result: " + result);
-                    return result * 2;
-                }).thenCompose(r -> CompletableFuture.supplyAsync(
-                        () -> r * 22))
-                .handle((r, ex) -> {
-                    if(ex != null){
-                        ex.getMessage();
-                    }
-                    return r;
-                }).thenAccept(result -> {
-                    System.out.println("Final result: " + result);
-                });
-
-        log.info("Приложение запущено!");
-        log.debug("Это отладочное сообщение.");
-        log.error("Ошибка: что-то пошло не так.");
-
-        System.out.println("Main thread is free to do other things...");
+//
+//        CompletableFuture.supplyAsync(() -> {
+//                    System.out.println("Task running in: " + Thread.currentThread().getName());
+//                    return 42;
+//                }).thenApply(result -> {
+//                    System.out.println("Transforming result: " + result);
+//                    return result * 2;
+//                }).thenApply(result -> {
+//                    System.out.println("Transforming result: " + result);
+//                    return result * 2;
+//                }).thenCompose(r -> CompletableFuture.supplyAsync(
+//                        () -> r * 22))
+//                .handle((r, ex) -> {
+//                    if(ex != null){
+//                        ex.getMessage();
+//                    }
+//                    return r;
+//                }).thenAccept(result -> {
+//                    System.out.println("Final result: " + result);
+//                });
+//
+//        log.info("Приложение запущено!");
+//        log.debug("Это отладочное сообщение.");
+//        log.error("Ошибка: что-то пошло не так.");
+//
+//        System.out.println("Main thread is free to do other things...");
     }
 }
